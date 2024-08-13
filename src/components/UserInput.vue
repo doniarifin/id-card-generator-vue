53838c43409284b1ef1d9c3d704fc11a209d333f
dhoni <dhoni@kanosolution.com>
2024-08-13 10:18:06 +0700

    fixing bg

--- src/components/UserInput.vue
+++ src/components/UserInput.vue
@@ -3,64 +3,64 @@
     <div class="mb-2">
       <h1 class="font-medium">ID Card Generator</h1>
     </div>
-    <div class="p-3 bg-gray-100 rounded-lg dark:bg-transparent">
+    <div class="p-3 bg-gray-100 rounded-lg">
       <form @submit.prevent="handleSubmit">
         <div class="grid gap-6 mb-6 md:grid-cols-2">
           <div>
-            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-700">
               First name
             </label>
             <input v-model="formData.first_name" type="text" id="first_name"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-700 dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="John" required />
           </div>
           <div>
-            <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label for="last_name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-700">
               Last name
             </label>
             <input v-model="formData.last_name" type="text" id="last_name"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-700 dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Doe" required />
           </div>
           <div>
-            <label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label for="company" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-700">
               Company
             </label>
             <input v-model="formData.company" type="text" id="company"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-700 dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="Flowbite" required />
           </div>
           <div>
-            <label for="number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label for="number" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-700">
               Phone number
             </label>
             <input v-model="formData.phone" type="number" id="phone"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-700 dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="123-45-678" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}" required />
           </div>
           <div>
-            <label for="website" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label for="website" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-700">
               Website URL
             </label>
             <input v-model="formData.website" type="text" id="website"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-700 dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="flowbite.com" required />
           </div>
           <div>
-            <label for="City" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            <label for="City" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-700">
               City
             </label>
             <input v-model="formData.city" type="text" id="City"
-              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-700 dark:focus:ring-blue-500 dark:focus:border-blue-500"
               placeholder="New York" required />
           </div>
         </div>
         <div class="mb-6">
-          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-700">
             Email address
           </label>
           <input v-model="formData.email" type="text" id="email"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-gray-700 dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="john.doe@company.com" required />
         </div>
         <button type="submit"
